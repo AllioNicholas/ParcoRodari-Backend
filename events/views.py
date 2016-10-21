3f132bb9fbf790c48d5558e76757445299f293c5
Nicholas <nicholas.allio@me.com>
2016-10-21 12:54:42 +0200

    implemented detail view and edit link

--- events/views.py
+++ events/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
+from .models import Event
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the events index.")
+
+def detail(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    return render(request, 'events/detail.html', {'event': event})
+
+def index(request):
+    events = get_list_or_404(Event)
+    return render(request, 'events/index.html', {'event_list': events})
+
+def edit(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    return render(request, 'events/edit.html', {'event': event})
